1f8ef49464136669cbf8e667193e55f890ea2764
Dan Bechard <dbechrd@gmail.com>
2019-09-01 18:11:08 -0700

    Fix delta time

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,18 @@
 #pragma comment(lib, "SDL2.lib")
 #pragma comment(lib, "SDL2main.lib")
 
-Game* game = new Game;
-
-const float PHYSICS_DT = 50;  // milliseconds
-float dt = 0.0f;
-float physics_accum = 0.0f;
-float dt_prev = 0.0f;
-
 int main(int argc, char* args[])
 {
-	if (!InitializeGame(game))
+    Game* game = new Game;
+    if (!InitializeGame(game))
 	{
 		LOG_FATAL("Error starting Game. See logs!");
 		return(-1);
 	}
 
-	dt_prev = SDL_GetTicks();
+    const uint32_t PHYSICS_DT = 50;  // milliseconds
+    uint32_t physics_accum = 0;
+    uint32_t last_frame = SDL_GetTicks();
 
 	while (IsRunning(game))
 	{
@@ -29,19 +25,21 @@ int main(int argc, char* args[])
 		//a delta time?
 		//UpdateDelta(&game->DeltaClock);
 
-		dt = SDL_GetTicks() - dt_prev;
-		dt_prev = dt;
+        uint32_t now = SDL_GetTicks();
+        uint32_t dt = now - last_frame;
+        last_frame = now;
+
 		physics_accum += dt;
 		while (physics_accum >= PHYSICS_DT)
 		{
-			Update(game, PHYSICS_DT);
+			Update(game, PHYSICS_DT / 1000.0f);
 			physics_accum -= PHYSICS_DT;
 		}
 		HandleInput(game);
-		Draw(game, dt);
+		Draw(game, dt / 1000.0f);
 
-		LOG_INFO("DT: {0}", dt);
-	}
+        LOG_INFO("DT: {0}", dt);
+    }
 
 	ShutdownGame(game);
 
