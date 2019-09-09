1fb7d24286301b67cbd2ecc191b48b09b7d7a230
GunnDawg <cgunn86@gmail.com>
2019-09-08 21:36:31 -0700

    Changed name/label on the Logger

--- src/P1_Log.cpp
+++ src/P1_Log.cpp
@@ -7,9 +7,9 @@ void Logger::Init()
 {
 	spdlog::set_pattern("%^[%T] %n: %v%$");
 
-	sCoreLogger = spdlog::stdout_color_mt("Engine");
+	sCoreLogger = spdlog::stdout_color_mt("Log");
 	sCoreLogger->set_level(spdlog::level::trace);
 
-	sFileLogger = spdlog::basic_logger_mt("Log Info", "log.txt");
+	sFileLogger = spdlog::basic_logger_mt("FileLog", "log.txt");
 	sFileLogger->set_level(spdlog::level::trace);
 }
